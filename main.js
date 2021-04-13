22fea5ec93a74fc5fa7ae7e081cae20eeb92bccc
Шелков Дмитрий <bobyorrr@gmail.com>
2021-04-13 12:46:22 +0300

    + иконка оффес

--- main.js
+++ main.js
@@ -61,6 +61,7 @@ const obektyMapGroupName = {
 		icon: "",
 	},
 };
+const obektyMapActiveClass = "active";
 
 ymaps.ready(function () {
 	for (const mapWrap of document.querySelectorAll(".obekty-map")) {
@@ -88,9 +89,9 @@ ymaps.ready(function () {
 					};
 					this.getData().options.set("shape", bigShape);
 					if (this.isActive) {
-						element.classList.add("active");
+						element.classList.add(obektyMapActiveClass);
 					} else if (this.inited) {
-						element.classList.remove("active");
+						element.classList.remove(obektyMapActiveClass);
 					}
 					if (!this.inited) {
 						this.isActive = false;
@@ -139,6 +140,17 @@ ymaps.ready(function () {
 			list: {
 				all: [],
 			},
+			offes: {
+				icon: ymaps.templateLayoutFactory.createClass(`
+					<svg style="transform: translateY(-100%);" width='40' height='59' viewBox="0 0 29 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<rect x="0.5" y="1.5" width="2" height="41" fill="#191B1E" stroke="white"/>
+						<path d="M3.19874 1.54119L2.5 1.23853V2V21.6635V22.4267L3.1997 22.1219L25.8324 12.2619L26.8876 11.8022L25.8315 11.3447L3.19874 1.54119Z" fill="#191B1E" stroke="white"/>
+						<path d="M10.2594 17.4941L8.17694 18.3976L6.49573 13.9365V19.1318L4.04724 20.1976V3.44116L6.49573 4.50587V9.19293L7.91997 5.12469L10.2012 6.11175L11.5503 14.4388L12.726 7.20469L17.9733 9.48116V10.5329L14.9418 9.21881L14.8157 9.99646L17.9733 11.3694V14.1341L13.8497 15.9376L14.2254 13.6247L16.2836 12.72L13.8497 11.6623L13.1018 16.2565L10.3248 17.4647L8.63876 8.99528L7.8133 11.2894L10.2594 17.4941Z" fill="white"/>
+						<path d="M18.8473 9.86118L23.3285 11.8012L18.8473 13.7482V9.86118Z" fill="#F4691B"/>
+					</svg>
+					`),
+				coordinates: null,
+			},
 			created() {
 				for (const icon of mapWrap.querySelectorAll(".markers > .grouped-map-marker")) {
 					const iconOptions = {
@@ -156,11 +168,12 @@ ymaps.ready(function () {
 				for (const key in this.list) {
 					if (Object.hasOwnProperty.call(this.list, key)) this.list.all.push(...this.list[key]);
 				}
+
+				const offesItem = mapWrap.querySelector(".markers > .map-marker");
+				this.offes.coordinates = [offesItem.dataset.latitude, offesItem.dataset.longitude];
 			},
 		};
 		obektyMapIcon.created();
-		console.log(obektyMapIcon.list);
-		console.log(obektyMapGroupName);
 
 		function obektyListItem(params) {
 			params = Object.assign(
@@ -173,12 +186,12 @@ ymaps.ready(function () {
 				},
 				params
 			);
-			// Выбор категории меток
+			// Рендер кнопок категории
 			let item = document.createElement("li");
 			item.className = "obekty-list__item";
 			item.innerHTML = `
 				<label class="obekty-list__button">
-					<input class="obekty-list__input" type="radio" name="obekty-list" />
+					<input class="obekty-list__input" type="radio" name="obekty-list" value="${params.key}" />
 					<img src="${params.iconUrl}" width="40" height="40" alt="${params.text}" class="obekty-list__button-icon">
 					<span class="obekty-list__button-text">${params.text}</span>
 				</label>
@@ -186,12 +199,14 @@ ymaps.ready(function () {
 			item.querySelector(".obekty-list__input").addEventListener("change", () => {
 				myMap.geoObjects.removeAll();
 				params.callback.apply(null, [params.key]);
+				addIconOffesMap();
 			});
 
 			params.wrap.insertAdjacentElement("beforeend", item);
 		}
 
 		function addIconMap(key) {
+			// Вывод иконок на карту
 			for (const icon of obektyMapIcon.list[key]) {
 				const myPlacemark = new ymaps.Placemark(
 					icon.coordinates,
@@ -207,6 +222,17 @@ ymaps.ready(function () {
 			}
 		}
 
+		function addIconOffesMap() {
+			const myPlacemark = new ymaps.Placemark(
+				obektyMapIcon.offes.coordinates,
+				{},
+				{
+					iconLayout: obektyMapIcon.offes.icon,
+				}
+			);
+			myMap.geoObjects.add(myPlacemark);
+		}
+
 		for (const key in obektyMapIcon.list) {
 			if (Object.hasOwnProperty.call(obektyMapIcon.list, key)) {
 				obektyListItem({
@@ -215,5 +241,14 @@ ymaps.ready(function () {
 				});
 			}
 		}
+
+		document.querySelectorAll(".obekty-list .obekty-list__button").forEach((item, index, list) => {
+			const input = item.querySelector(".obekty-list__input");
+			input.addEventListener("change", () => {
+				for (const itemList of list) itemList.classList.remove(obektyMapActiveClass);
+				item.classList.add(obektyMapActiveClass);
+			});
+			if (input.value === "all") item.click();
+		});
 	}
 });
